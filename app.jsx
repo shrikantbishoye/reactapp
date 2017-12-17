d5762bf9a18fa98f28083aa9899dd7947c45ca3c
shrikant <shrikantbishoye@gmail.com>
2017-12-18 02:59:56 +0530

    learn how to style and comment etc

--- app.jsx
+++ app.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
-
 class hello extends React.Component {
 	render() {
+		var  myFirstStyle = {
+			color: '#f00',			
+			textAlign: 'center',
+			fontSize: 30,
+		}
 		return (
-			<div> Hello </div>
+			<div>
+	            <h1 style={myFirstStyle}>Header</h1>
+	            <h2>Content</h2>
+	            <p data-myattribute = "somevalue">This is the content!!!</p>
+	            
+	        	{/*Multi line comment in react style*/}
+	        </div>
 		);
 	}
 }
