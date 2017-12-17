1e1ff173873da55e7b835421de846a1cd399c77a
shrikant <shrikantbishoye@gmail.com>
2017-12-18 03:12:26 +0530

    state full comonent added

--- app.jsx
+++ app.jsx
@@ -1,22 +1,52 @@
 import React from 'react';
-class hello extends React.Component {
-	render() {
-		var  myFirstStyle = {
-			color: '#f00',			
-			textAlign: 'center',
-			fontSize: 30,
-		}
+
+
+class myReactApp extends React.Component {
+    render() {
 		return (
+		    <div>
+		        <Header/>
+		        <Content/>
+		        <Footer/>
+		    </div>
+		);
+    }
+}
+
+
+class Header extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>Header</h1>
+         </div>
+      );
+   }
+}
+
+
+class Content extends React.Component {
+   render() {
+      return (
+         <div>
+            <h2>Content</h2>
+            <p>The content text!!!</p>
+         </div>
+      );
+   }
+}
+
+class Footer extends React.Component {
+	render() {
+		return(
 			<div>
-	            <h1 style={myFirstStyle}>Header</h1>
-	            <h2>Content</h2>
-	            <p data-myattribute = "somevalue">This is the content!!!</p>
-	            
-	        	{/*Multi line comment in react style*/}
-	        </div>
+				<p>
+					Footer 
+				</p>
+			</div>
 		);
 	}
 }
 
-export default hello;
+export default myReactApp;
 
